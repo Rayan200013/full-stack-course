e7b3f8ac8d16e16dc961b2067252ab2ea9ef6ed4
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-07-05 16:38:28 +0300

    rayan commit

---
+++ day1/script.js
@@ -0,0 +1,25 @@
+console.log("HI");
+
+let fname = "Rayan";
+fname = "rayn";
+console.log(fname);
+
+const PI = Math.PI;
+console.log(PI);
+console.log(Math.round(PI));
+
+const numbers = [0, 12, 25, 37, 98.7, 100];
+const string = ["Rayan", "ray", "ray1", "rro", "rorz"];
+
+let js = "javascript";
+const charsInJavascript = js.split("");
+
+console.log(charsInJavascript);
+
+console.log("Hi's\n ki\nfa\nk");
+
+let string1 = "30 dys of javascript";
+
+console.log(string1.includes("dys"));
+console.log(string1.includes("30"));
+console.log(string1.includes("o"));
